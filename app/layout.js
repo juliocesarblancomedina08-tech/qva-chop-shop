6d53e5e0c3fed401895cd9814c44a34a499aa25a
juliocesarblancomedina08-tech <juliocesarblancomedina08@gmail.com>
2026-08-29 12:33:14 -0400

    Add global cart button

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import CartButton from "./CartButton";
 
 export const metadata = {
   title: "Qva🇨🇺CHOP 🛒",
@@ -8,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body>
+        <CartButton />
+        {children}
+      </body>
     </html>
   );
 }
