13029c43d8cd7071eb792c04c7db7859aac54613
juliocesarblancomedina08-tech <juliocesarblancomedina08@gmail.com>
2026-08-29 10:41:55 -0400

    Create home page

---
+++ app/page.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>🎮 Qva🇨🇺CHOP 🛒</h1>
+
+      <h2>💎 100 — 2200💎 Diamond Singapur</h2>
+
+      <p>Selecciona una oferta para continuar.</p>
+
+      <button>Ver ofertas</button>
+    </main>
+  );
+}
